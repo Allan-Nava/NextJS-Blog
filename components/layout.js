16ad5e39aac213635ba89a161129c09a94c00690
Allan Nava <allannava95@gmail.com>
2020-11-03 10:38:36 +0100

    Update layout.js

--- components/layout.js
+++ components/layout.js
@@ -1,4 +1,11 @@
-
+/*
+ *  layout.js
+ *  Allan Nava 2020-11-03
+ *  Allan Nava 2020-11-03
+ *
+ *  Created by [ Allan Nava ].
+ *  Updated by [ Allan Nava ]
+ */
 export default function Layout({ children }) {
     return <div>{children}</div>
 }
