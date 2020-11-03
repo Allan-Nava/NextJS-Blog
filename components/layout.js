3c5d5dbdc256ea10f702837e43b33f79768851ea
Allan Nava <allannava95@gmail.com>
2020-11-03 18:18:51 +0100

    Update layout.js

--- components/layout.js
+++ components/layout.js
@@ -6,6 +6,8 @@
  *  Created by [ Allan Nava ].
  *  Updated by [ Allan Nava ]
  */
+import styles from './layout.module.css'
+///
 export default function Layout({ children }) {
-    return <div>{children}</div>
+  return <div className={styles.container}>{children}</div>
 }
