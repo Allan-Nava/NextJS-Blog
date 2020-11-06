2c43f56a1a02b2fec1df8a69ae05b09abf775ee5
Allan Nava <allannava95@gmail.com>
2020-11-06 14:06:57 +0100

    Update _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -1,3 +1,11 @@
+/*
+ *  _app.js
+ *  Allan Nava 2020-11-06
+ *  Allan Nava 2020-11-06
+ *
+ *  Created by [ Allan Nava ].
+ *  Updated by [ Allan Nava ]
+ */
 import '../styles/index.css'
 
 export default function MyApp({ Component, pageProps }) {
