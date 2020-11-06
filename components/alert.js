a1d395507e236f43bbe0c0f3accaa1e30d7aa9c7
Allan Nava <allannava95@gmail.com>
2020-11-06 14:31:26 +0100

    Update alert.js

--- components/alert.js
+++ components/alert.js
@@ -6,10 +6,10 @@
  *  Created by [ Allan Nava ].
  *  Updated by [ Allan Nava ]
  */
- import Container from './container'
+import Container from './container'
 import cn from 'classnames'
 import { EXAMPLE_PATH } from '../lib/constants'
-
+//
 export default function Alert({ preview }) {
   return (
     <div
