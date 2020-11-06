303c374160774d36b6534ad6a5fdf6498a12ceb8
Allan Nava <allannava95@gmail.com>
2020-11-06 14:12:19 +0100

    Update layout.js

--- components/layout.js
+++ components/layout.js
@@ -1,15 +1,25 @@
 /*
  *  layout.js
  *  Allan Nava 2020-11-03
- *  Allan Nava 2020-11-03
+ *  Allan Nava 2020-11-06
  *
  *  Created by [ Allan Nava ].
  *  Updated by [ Allan Nava ]
  */
-import styles from './layout.module.css'
 ///
-const name = 'Allan Nava';
-///
-export default function Layout({ children }) {
-  return <div className={styles.container}>{children}</div>
+import Alert from '../components/alert'
+import Footer from '../components/footer'
+import Meta from '../components/meta'
+
+export default function Layout({ preview, children }) {
+  return (
+    <>
+      <Meta />
+      <div className="min-h-screen">
+        <Alert preview={preview} />
+        <main>{children}</main>
+      </div>
+      <Footer />
+    </>
+  )
 }
